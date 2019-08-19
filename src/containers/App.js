6f9737fc87a6a5e005f0c62a4fcd2c2457a31072
Roman Kiss <romiki44@hotmail.com>
2019-08-19 07:49:27 +0200

    csometics fixs

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,14 +13,14 @@ const mapStateToProps=state=>{
     robots: state.requestRobots.robots,
     isPending: state.requestRobots.isPending,
     error: state.requestRobots.error
-  }
+  };
 };
 
 const mapDispatchToProps=(dispatch)=>{
   return {
     onSearchChange: (event)=>dispatch(setSearchField(event.target.value)),
     onRequestRobots: ()=>dispatch(requestRobots())
-  }
+  };
 };
 
 class App extends Component {
@@ -32,18 +32,16 @@ class App extends Component {
     const {searchField, onSearchChange, robots, isPending} = this.props;
     const filteredRobots = robots.filter(robot =>{
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
-    })
+    });
     return isPending ?
       <h1>Loading</h1> :
-      (
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
           <SearchBox searchChange={onSearchChange}/>
           <Scroll>
             <CardList robots={filteredRobots} />
           </Scroll>
-        </div>
-      );
+        </div>;
   }
 }
 
